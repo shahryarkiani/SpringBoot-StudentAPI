92259088d69e3a171cbcaf993c7a7974638e3b03
ohnomytoast <shahryarkiani04@gmail.com>
2022-09-05 22:31:48 -0400

    Add Delete Mapping

--- src/main/java/com/example/springboottut1/student/StudentController.java
+++ src/main/java/com/example/springboottut1/student/StudentController.java
@@ -26,10 +26,23 @@ public class StudentController{
     {
         return studentService.getStudents();
     }
-
+    
     @PostMapping
     public void registerNewStudent(@RequestBody Student newStudent)
     {
         studentService.addNewStudent(newStudent);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId)
+    {
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Long studentId, @RequestParam(required = false) String name,
+                              @RequestParam(required = false) String email)
+    {
+        studentService.updateStudent(studentId, name, email);
     }
 }
--- src/main/java/com/example/springboottut1/student/StudentService.java
+++ src/main/java/com/example/springboottut1/student/StudentService.java
@@ -3,9 +3,13 @@ package com.example.springboottut1.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.beans.Transient;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -23,6 +27,40 @@ public class StudentService {
 
     public void addNewStudent(Student student)
     {
-        System.out.println(student);
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if(studentOptional.isPresent())
+        {
+            throw new IllegalStateException("Email already in use");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long id) {
+        boolean exists = studentRepository.existsById(id);
+        if(!exists)
+        {
+            throw new IllegalStateException("Student with ID " + id + " does not exist");
+        }
+        studentRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void updateStudent(Long id, String name, String email)
+    {
+        Student student = studentRepository.findById(id).orElseThrow(() -> new IllegalStateException("Student with ID " +
+                id + " does not exist"));
+        if(name != null && name.length() > 0 && !Objects.equals(student.getName(), name))
+        {
+            student.setName(name);
+        }
+
+        if(email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email))
+        {
+            if(studentRepository.findStudentByEmail(email).isPresent())
+            {
+                throw new IllegalStateException("Email is already in use");
+            }
+            student.setEmail(email);
+        }
     }
 }
